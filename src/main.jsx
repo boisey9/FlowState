fbfadb3a2fd561e36b4f31587542cbaba0ac1d43
boisey9 <erikboisvert9@gmail.com>
2026-05-22 15:25:59 -0400

    Disable Trade Banana DOM rebrand helper

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TradeBananaRebrand from './components/TradeBananaRebrand.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TradeBananaRebrand />
     <App />
   </React.StrictMode>,
 )
