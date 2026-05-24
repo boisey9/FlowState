4ca1018e291167e20238e8a9423f79001a774502
boisey9 <erikboisvert9@gmail.com>
2026-05-24 16:18:21 -0400

    Load mobile navigation clearance fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './trade-banana-theme.css'
+import './trade-banana-mobile-fixes.css'
 import TradeBananaRouter from './TradeBananaRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
