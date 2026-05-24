5d484af90b05df8c1a2e945512603f6184bf8660
boisey9 <erikboisvert9@gmail.com>
2026-05-24 15:02:32 -0400

    Use Trade Banana router for Analyze and Watchlist

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './trade-banana-theme.css'
-import App from './App.jsx'
+import TradeBananaRouter from './TradeBananaRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TradeBananaRouter />
   </React.StrictMode>,
 )
