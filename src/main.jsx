83fbcbc9704fe10c632d0fc93d5d822f7d6ff63a
boisey9 <erikboisvert9@gmail.com>
2026-05-26 11:15:50 -0400

    Disable broken Banana State Map image override

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './trade-banana-theme.css'
-import './banana-state-illustrations.css'
 import './trade-banana-mobile-fixes.css'
 import TradeBananaRouter from './TradeBananaRouter.jsx'
 
